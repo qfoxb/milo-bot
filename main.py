bac43c6f9fb24b94c512bfa0c6cf00e21d540a75
femou <git@femou.dev>
2023-12-14 14:59:49 -0400

    on_ready

--- main.py
+++ main.py
@@ -72,6 +72,10 @@ intents.message_content = True
 client = discord.Client(intents=intents)
 
 @client.event
+async def on_ready():
+    print(f'Bot has logged in as {client.user}.')
+
+@client.event
 async def on_message(message):
     if message.author == client.user:  
         return
