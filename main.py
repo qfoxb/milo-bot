42cd6c1b95658e18849536042470fc5b5cd91a2a
femou <git@femou.dev>
2023-12-14 15:03:54 -0400

    message.author.bot

--- main.py
+++ main.py
@@ -79,6 +79,8 @@ async def on_ready():
 async def on_message(message):
     if message.author == client.user:  
         return
+    if message.author.bot:
+        return
     
     for mentions in message.mentions:
         if mentions == client.user:
